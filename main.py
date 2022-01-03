45d87ab3c5948f8f1b8d42f2cca1c4a14b458493
goransavich <goran.savich@yahoo.com>
2022-01-03 22:37:50 +0100

    third commit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.uix.button import Button
 from kivy.lang.builder import Builder
 from plyer import filechooser
 import pathlib
@@ -12,12 +15,27 @@ Window.size = (350,550)
 
 class StambiljWidget(Widget):
     def ucitaj(self):
-        imported_file = filechooser.open_file(title="Izaberi prijavu",
-                                         filters=["*.pdf", "*.bmp", "*.jpg","*.jpeg", "*.png"])
+        imported_file = filechooser.open_file(title="Izaberi prijavu")
 
+        list_of_extensions = [".pdf", ".bmp", ".jpg",".jpeg", ".png"]
+        ##preventing crashing app if user not load any file
         if len(imported_file) !=0:
             file_extension = pathlib.Path(imported_file[0]).suffix
 
+        if (file_extension not in list_of_extensions):
+            # create content and add to the popup
+            content = Button(text='Zatvori!')
+            popup = Popup(title="Nije dobar format fajla.",
+                          content=content, auto_dismiss=False,
+
+                          size_hint=(None, None), size=(300, 200)
+                          )
+
+            # bind the on_press event of the button to the dismiss function
+            content.bind(on_press=popup.dismiss)
+
+            # open the popup
+            popup.open()
 
     def zavrsi(self):
         datum = self.ids.datum_prijave.text
